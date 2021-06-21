9405bce297c5a1aa52a685623a16c22ccc2b10c6
Jess <jmvsevilleja@gmail.com>
2021-06-21 14:59:05 +0800

    Added firebase and state management

---
+++ client/firebase.js
@@ -0,0 +1,17 @@
+import Firebase from 'firebase/app'
+import 'firebase/auth'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBfiyrzWmx4dXMcE2w_9NS1n4FrXj-dFmM",
+    authDomain: "booking-92461.firebaseapp.com",
+    projectId: "booking-92461",
+    storageBucket: "booking-92461.appspot.com",
+    messagingSenderId: "640642507010",
+    appId: "1:640642507010:web:06603757ee95b5ac67621b",
+    measurementId: "G-BZSQY77G8N"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default frirebase
